d12d15c7835987591ec6100662a4d650358aef6f
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-06-17 12:05:12 +0000

    Add files via upload

---
+++ Solutions/Depth_First_Search.py
@@ -0,0 +1,43 @@
+"""We are given a directed graph, and we want to return the list of
+
+it's traversal using DFS"""
+
+##Example
+
+#  graph={
+#         a:['c','b'],
+#         b: ['d'],
+#         c:  ['e'],
+#         d:  ['f'],
+#         e:  [],
+#         f:  []
+#        }
+
+
+## Output: ['a','b','d','f','c','e']
+
+class solution(object):
+    
+    def dfs(self,graph,source):
+
+        ### initialize a stack with source node
+
+        stack=[source]
+
+        while stack !=None:
+
+            ## current node = node in stack
+
+            curr= stack.pop()
+
+            for nodes in graph[curr]:
+
+                stack.append(nodes)
+        
+
+        return stack
+
+### complexity analysis: 
+
+## Time O(N) with N= len(grapg)
+## Space complexity O(1)
