23614e81bf461686e997a7e388fa39daee88a6cf
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-06-28 07:01:08 +0000

    Add files via upload

---
+++ Solutions/binary_tree_dfs.py
@@ -0,0 +1,35 @@
+"""
+Here we're creating a dfs algorithm for binary tree
+As usual, we're using a stack for dfs and queue for bfs
+"""
+
+from asyncio.windows_events import NULL
+
+
+class Tree(object):
+
+    def __init__(self,val=0,left=None,right=None):
+
+        self.left=None
+        self.right=None
+        self.val=0
+
+class dfs(object):
+
+    def solution(self,root):
+
+        if root ==NULL: return []
+        result=[]
+        stack=[root]
+
+
+        while len(stack)!=0:
+
+            curr=stack.pop()
+
+            result.append(curr.left)
+            result.append(curr.right)
+
+        return result
+
+        
