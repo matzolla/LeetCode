71289347d229d6cfe21bfc4eb372212066f80db9
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-08-06 07:36:31 +0000

    Add files via upload

---
+++ Solutions/linked_list.py
@@ -0,0 +1,26 @@
+"""
+Traversing a linked list
+
+"""
+
+class node():
+
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+
+def printlinkedLIst(head):
+    curr =head
+
+    while curr!=None:
+
+        print(curr)
+
+        curr=curr.next
+
+def recursivelinkedlist(head):
+    curr=head
+    if curr==None: return 0
+    print(curr.val)
+
+    return recursivelinkedlist(curr.next)
