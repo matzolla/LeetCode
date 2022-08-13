f3ac6b833b67065721dfc574fc9b6a24d419a3d1
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-08-13 20:45:35 +0000

    Add files via upload

---
+++ Solutions/valid_anagram.py
@@ -0,0 +1,22 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Input: s = "rat", t = "car"
+Output: false
+"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s)==sorted(t)
+
+
+## time complexiity: O(nlogn)
+## space complexity : O(1)
