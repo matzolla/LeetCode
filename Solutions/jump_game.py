9f02a7f362594737a1269cebcfaf59d3779e3416
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-08-16 12:43:40 +0000

    Create jump_game.py

---
+++ Solutions/jump_game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        jump=0
+        
+        for i in range(len(nums)):
+            if jump< i : return False ### meaning we can't jump
+            
+            jump= max(jump,i+nums[i])
+            
+        return True
+   
+  
+  ## Space complexity O(1)
+  # time O(n)
