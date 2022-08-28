bb8e2293f95ccd640c7bb14e807e1719aa83373a
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-08-28 20:52:31 +0000

    Create maximum_subarray.py

---
+++ Solutions/maximum_subarray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curr_sum=0
+        largest_sum=-float('inf')
+        
+        if len(nums)==1: return nums[0]
+        
+        for i in range(len(nums)):
+            
+            curr_sum=max(curr_sum+nums[i],nums[i])
+            
+            if curr_sum>largest_sum:
+                largest_sum=curr_sum
+            
+        return largest_sum
+      
+### time complexity O(n), space complexity O(1)
