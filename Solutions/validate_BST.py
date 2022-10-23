165570ca0f41a1658ee1f7feac08699bab1d3d96
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-10-23 00:24:12 +0000

    Create validate_BST.py

---
+++ Solutions/validate_BST.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack =[(root,float('inf'),-float('inf'))]
+        
+        while len(stack)>0:
+            
+            curr,_max,_min=stack.pop()
+            if curr.val>=_max or curr.val<=_min:
+                return False
+            if curr.right:
+                stack.append((curr.right,_max,max(_min,curr.val)))
+            if curr.left:
+                stack.append((curr.left,min(_max,curr.val),_min))
+        return True
+              #   '''
+              #   5
+              #  / \
+              # 4   6
+              #    / \ 
+              #   3   7
+              #   '''
+              # Time complexity O(N)
+             ## space complexity O(h)
