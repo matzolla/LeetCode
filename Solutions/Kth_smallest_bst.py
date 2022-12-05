83062e03a40c5826acd2edf0d9d48912db3510e3
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-12-05 13:15:30 +0000

    Create Kth_smallest_bst.py

---
+++ Solutions/Kth_smallest_bst.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        inorder traversal= []
+        """
+        
+        ## The idea is to traverse the tree using either bfs or dfs and append 
+        ## the values of the array  time complexity will be O(nlogn)
+        
+        ## the hint require us to use the property of bst to optimize. Let's see
+        ## using inorder traversal time complexity O(N), space O(N)
+        
+        stack=[root]
+        curr=root
+        nodes=[]
+        while curr or len(stack)>1:
+            
+            while curr:
+                stack.append(curr)
+                curr=curr.left
+                
+            curr=stack.pop()
+            nodes.append(curr.val)
+            curr=curr.right
+            
+        return nodes[k-1]
