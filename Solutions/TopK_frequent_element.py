5500735b0842bcd7af79731e5de68b71f02cd8d3
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2022-12-30 23:32:24 +0000

    Create TopK_frequent_element.py

---
+++ Solutions/TopK_frequent_element.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        dictionary={}
+        count=0
+        result=[]
+        for key in nums:
+
+            if key not in dictionary:
+                dictionary[key]=1
+            else:
+                dictionary[key]+=1
+
+        pairs=[(-1*value,key) for (key,value) in dictionary.items()]
+
+        #defining a heap
+        
+        heapq.heapify(pairs)
+
+        while count < k:
+            value,key= heapq.heappop(pairs)
+            result.append(key)
+            count+=1
+        return result
