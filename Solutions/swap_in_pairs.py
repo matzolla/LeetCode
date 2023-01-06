8c9d7a29ac034ee0cd1d1f9a5b0469e539714c51
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-01-06 14:14:30 +0000

    Create swap_in_pairs.py

---
+++ Solutions/swap_in_pairs.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# time complexity O(N), space complexity O(1)
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        dummylist= ListNode()
+        ## attaching the head to the dummylist
+        dummylist.next=head
+        prevnode=dummylist
+        currnode=head
+
+        while currnode and currnode.next:
+            prevnode.next=currnode.next
+            currnode.next=prevnode.next.next
+            prevnode.next.next=currnode
+
+            prevnode=currnode
+
+            currnode=currnode.next
+
+        return dummylist.next
