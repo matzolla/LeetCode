64e82f50e6ec0ac3134565fb3a8186070948000e
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-01-10 11:44:55 +0000

    Create adding_two_linked_list.py

---
+++ Solutions/adding_two_linked_list.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummyHead=ListNode()
+        curr=dummyHead
+        carry=0
+
+        while l1 and l2:
+            value= l1.val + l2.val + carry
+
+            carry=value%10
+
+            curr.next=ListNode(carry)
+            carry=value//10
+            l1=l1.next
+            l2=l2.next
+            curr=curr.next
+        
+        while l1:
+            value=l1.val+ carry
+            carry=value%10
+            curr.next=ListNode(carry)
+            carry=value//10
+            l1=l1.next
+            curr=curr.next
+        
+        while l2:
+            value=l2.val+ carry
+            carry=value%10
+            curr.next=ListNode(carry)
+            carry=value//10
+            l2=l2.next
+            curr=curr.next
+        if carry!=0:
+            curr.next=ListNode(carry)
+
+        return dummyHead.next
