49a3cb98f6700970f2068c3392fb1cafa393d4b7
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-01-22 16:19:48 +0000

    Create array_partition.py

---
+++ Solutions/array_partition.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        """
+
+        ## we can use countsort
+        ## time complexity O(nlogn)
+
+        ## but lets use sort here
+
+        nums.sort()
+
+        window_size=2
+
+        result=0
+        for idx in range(0,len(nums),2):
+
+            result+=min(nums[idx:idx+window_size+1])
+        
+        return result
