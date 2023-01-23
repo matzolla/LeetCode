4ea825f1a194fdc36f2d0d70eee797c3668b142e
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-01-23 12:14:09 +0000

    Add files via upload

---
+++ Solutions/bit_manipulation.py
@@ -0,0 +1,30 @@
+class reverse_bit:
+    """
+    reversing a binary bit 
+    time complexity O(1)
+    """
+    def __init__(self,n):
+        self.n=n
+
+    def reverse(self):
+
+        res=0
+
+        for i in range(32):
+
+            bit=(self.n>>i)&1
+
+            res=res|(bit<<(32-i))
+
+        return  res
+
+
+rev=reverse_bit(32)
+
+# print(rev.reverse())
+
+
+
+
+
+
