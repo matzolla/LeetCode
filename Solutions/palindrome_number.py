965b264f553d4780a93c2a98a8e1ecf645719e3a
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-01-29 14:09:39 +0000

    Update palindrome_number.py

--- Solutions/palindrome_number.py
+++ Solutions/palindrome_number.py
@@ -7,11 +7,26 @@ class Solution(object):
         x=121
 
         without converting to string
-        
+
+        let's try to do without string
+        the idea is to reverse the last half of the number
+        and check if the first half and the reverted last half are equal
+
+        time complexity O(N)
+        space complexity O(1)
+        reverted=0
+
+        while x> reverted:
+
+            reverted= reverted*10 + x%10
+            x//=10 (and that's it)
+
+        return x == reverted || x == reverted//10
 
         """
         ## time complexity O(n)
-        ## space complexity O(1)
+        ## space complexity O(n) (it varies)
+
         str_x=str(x)
 
         left=0
