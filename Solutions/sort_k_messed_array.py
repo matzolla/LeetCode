f16cbe6881088dda00c728c30cd259675fe626ab
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-02-14 18:16:58 +0000

    Create sort_k_messed_array.py

---
+++ Solutions/sort_k_messed_array.py
@@ -0,0 +1,20 @@
+import heapq
+
+def sort_k_messed_array(arr, k):  
+
+  sub_array= arr[:k+1]
+  heapq.heapify(sub_array)  
+
+  for idx in range(k+1,len(arr)):        
+        curr_val= heapq.heappop(sub_array)
+        arr[idx-(k+1)]=curr_val
+        heapq.heappush(sub_array,arr[idx])
+
+  idy=len(arr)-k-1
+  while idy< len(arr):
+      curr_val= heapq.heappop(sub_array)
+      arr[idy]=curr_val
+
+      idy+=1
+
+  return arr
