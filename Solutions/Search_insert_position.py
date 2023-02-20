3095dc1df515d223e744e837ead876451ea5930d
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-02-20 20:30:24 +0000

    Create Search_insert_position.py

---
+++ Solutions/Search_insert_position.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        # """
+        ## three main conditions
+        ## time complexity O(logn)
+        ## space complexity O(1)
+        left = 0
+        right=len(nums)-1
+
+        while left<=right:
+
+            mid= (left+right)//2
+
+            if nums[mid]==target: return mid
+
+            if nums[mid]>target:
+                right=mid-1
+            else:
+                left=mid+1
+
+        return mid+1 if target>nums[mid] else mid
+        
