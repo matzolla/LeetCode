1f890b1871846abfe3463aa963d74f08a4b4ddb8
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-04-17 22:22:16 +0000

    Create longest_substring_without_repeating_char.py

---
+++ Solutions/longest_substring_without_repeating_char.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+       """
+       time complexity O(n)
+       """
+       left=0
+       right=0
+       seen=set()
+
+       longest_substring= -float('inf')
+       while left<len(s) and right<len(s):
+          if s[right] not in seen:
+             curr_val= (right-left)+1
+             seen.add(s[right])
+             right+=1
+          else:
+             
+             seen.remove(s[left])
+             left+=1
+
+          if curr_val>longest_substring:
+             longest_substring=curr_val
+          
+          
+
+       return longest_substring if len(s)>0 else 0
