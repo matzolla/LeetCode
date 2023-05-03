eb97653dd6707fd77f22602c6ac6e57a98685973
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-05-03 22:13:44 +0000

    Create diff_of_two_arrays.py

---
+++ Solutions/diff_of_two_arrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        
+        easy approach
+        """
+        set1=set(nums1)
+        set2=set(nums2)
+
+        return [list(set1.difference(set2)),list(set2.difference(set1))]
