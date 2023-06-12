734d9caaf8805413f0c12f74869cc9c01dde8189
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-06-12 23:14:56 +0000

    Create best_time_to_buy_and_sell_stocks.py

---
+++ Solutions/best_time_to_buy_and_sell_stocks.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+
+        if len(prices)==1:
+            return 0
+        buy_price=prices[0]
+        sell_price=prices[1]
+        
+        
+        profit= sell_price-buy_price
+        
+        for i in range(1,len(prices)):
+            
+            if buy_price> prices[i]: buy_price=prices[i]
+                
+            profit=max(profit,prices[i]-buy_price)
+            
+        return profit
