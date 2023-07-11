48f554c7eb580377608995e47ec504b3f7f52bb5
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-07-11 08:18:23 +0000

    Update Junior_question_solved.py

--- Solutions/Junior_question_solved.py
+++ Solutions/Junior_question_solved.py
@@ -1,13 +1,13 @@
-"""[6 7 4] 10
- 4 4  4 
-[5,3,2] 10
-left_over_wood=0"""
-
+### Time complexity O(N)
+### space complexity O(1)
 for length in wood:
    If length<= left_over_wood:
        “”” do nothing “”””
    else;
       “”increment””
    left_over_wood=max(left_over_wood, L-length)
-### Time complexity O(N)
-### space complexity O(1)
+"""   example : [6 7 4] 10
+                4 4  4 
+                [5,3,2] 10
+                left_over_wood=0
+"""
