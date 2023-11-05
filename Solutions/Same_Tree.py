f25dd5aaf49805d99a025b90c64c2d530cd95cbb
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-11-05 23:36:04 +0000

    Update Same_Tree.py

--- Solutions/Same_Tree.py
+++ Solutions/Same_Tree.py
@@ -10,22 +10,6 @@ class Solution(object):
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
-        
-        passed 58/60 test cases 
-        time complexity O(max(P,Q)), space O(max(p,q))
-        
-         I came to the conclusion that, I don't necessarily need dfs or bfs to 
-         solve all binary tree problems
-         
-         
-         
-         a simple way:
-         
-         if not p and not q: return True
-         if not p or not q: return False
-         if p.val!=q.val: return False
-         
-         return self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)
         """
         ### sanity checking
 
