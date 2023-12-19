db44dd2aa7908406eeeda92f39b8a6728941a67f
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-12-19 23:01:01 +0000

    Create flatten_nested_list.py

---
+++ Solutions/flatten_nested_list.py
@@ -0,0 +1,12 @@
+# flatten nested list
+# the code below is used to flatten nested list
+# for example input =[1,2,[3,[4,5]]]
+# output= [1,2,3,4,5]
+def flatten_nested_list(nested_list):
+    flattened_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flattened_list.extend(flatten_nested_list(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
