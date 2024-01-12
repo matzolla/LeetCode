7ad3f069c2ab71139df7817f0e0e86107015eb27
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2024-01-12 23:57:42 +0000

    Update Kth_positive.py

--- Solutions/Kth_positive.py
+++ Solutions/Kth_positive.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: int
 
-        [1,2,3,4,5,6,7,8,9,10,11]
-        idx=[0,1,2,3,4]
-        arr=[2,3,4,7,11]
+             [1,2,3,4,5,6,7,8,9,10,11]
+        idx= [0,1,2,3,4]
+        arr= [2,3,4,7,11]
+        
         """
         for idx in range(len(arr)):
 
