b8f5e353f4d3fe9f9314cc4bdef3d13e4055e3ce
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2024-02-02 23:54:59 +0000

    Update diff_of_two_arrays.py

--- Solutions/diff_of_two_arrays.py
+++ Solutions/diff_of_two_arrays.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[List[int]]
+
+        but still have to evaluate the time and space complexity
         
         """
         set1=set(nums1)
